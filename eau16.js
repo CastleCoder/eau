6c69af4d1e0d64491f70f58a36c7cff361557f6f
Cyrille <cyrillechateau@icloud.com>
2023-03-25 15:25:15 +0100

    finish

---
+++ eau16.js
@@ -0,0 +1,7 @@
+function waterFinish(){
+
+  console.log("J'ai terminé l'épreuve de l'eau, c'était assez long dans le sens où se mettre dans le bain pour réfléchir à une solution prend un certain temps.")
+
+}
+
+waterFinish();
